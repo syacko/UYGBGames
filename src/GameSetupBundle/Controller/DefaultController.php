513f2ac9e493355a1d4f746d1e4a63cb05d51b1b
Scott Yacko <scott@yackofamily.com>
2016-03-25 09:44:39 -0700

    Fixed bad file references Added jsLibrary variable to parameters.yml

--- src/GameSetupBundle/Controller/DefaultController.php
+++ src/GameSetupBundle/Controller/DefaultController.php
@@ -19,7 +19,8 @@ class DefaultController extends Controller
     {
 //        return $this->render('GameSetupBundle:Default:index.html.twig'); DO NOT USE
         $imgLocal = $this->container->getParameter("img_local");
+        $jsLibrary = $this->container->getParameter("js_library");
 //        echo $imgLocal;
-        return $this->render('GameSetupBundle:Default:gamesetup.html.twig', array('imgLocal' => $imgLocal));
+        return $this->render('GameSetupBundle:Default:gamesetup.html.twig', array('imgLocal' => $imgLocal, 'jsLibrary' => $jsLibrary));
     }
 }
